292c7aac511368712ed3064bc53e5c645e34b7dd
PietroGuardati <pietroguardati@gmail.com>
2021-03-18 00:33:08 +0100

    proto: intermediate step to extract matchable dataframes

--- src/create_test_data.py
+++ src/create_test_data.py
@@ -25,12 +25,12 @@ def sample_dataset(dataset_path):
         )
         df_climate_temp.to_csv(output_file, index=False)
     elif extension == "json":
-        df_climate_temp = pd.read_json(
+        df = pd.read_json(
             input_file,
             nrows=number_of_elements,
             lines=True
         )
-        df_climate_temp.to_json(
+        df.to_json(
             output_file,
             orient="records"
         )
@@ -38,21 +38,46 @@ def sample_dataset(dataset_path):
     print(f"storing dataset in {output_file}")
 
 
-def main():
-    """create test data"""
-    datasets = [
-        "climate_explorer/USC00420849-BOULDER-precipitation-inch.csv",
-        "climate_explorer/USC00420849-temperature-degreeF.csv",
-        "covid_19_dataset_2020_06_10/yelp_academic_dataset_covid_features.json",
-        "yelp_dataset/yelp_academic_dataset_business.json",
-        "yelp_dataset/yelp_academic_dataset_checkin.json",
-        "yelp_dataset/yelp_academic_dataset_review.json",
-        "yelp_dataset/yelp_academic_dataset_tip.json",
-        "yelp_dataset/yelp_academic_dataset_user.json"
-    ]
-    for dataset in datasets:
-        sample_dataset(dataset)
+def sample_reviews(dataset_path):
+    number_of_elements = 100
+    input_file = os.path.join(DIR_DATA, dataset_path)
+    output_file = os.path.join(DIR_DATA_TEST, dataset_path)
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
+    # sample and store file in test directory
+    df = pd.read_json(
+        input_file,
+        nrows=number_of_elements,
+        lines=True
+    )
+    df.to_json(
+        output_file,
+        orient="records"
+    )
+    reviews_id = list(df["review_id"].values)
+    business_id = list(df["business_id"].values)
+    user_id = list(df["user_id"].values)
+    date = list(df["date"].values)
+    return reviews_id, business_id, user_id, date
 
 
-if __name__ == "__main__":
-    main()
+#
+# def main():
+#     """create test data"""
+#
+#
+# if __name__ == "__main__":
+#     main()
+
+
+reviews_path = "yelp_dataset/yelp_academic_dataset_review.json"
+reviews_id, business_id, user_id, date = sample_reviews(reviews_path)
+datasets = [
+    "climate_explorer/USC00420849-BOULDER-precipitation-inch.csv",
+    "climate_explorer/USC00420849-temperature-degreeF.csv",
+    "covid_19_dataset_2020_06_10/yelp_academic_dataset_covid_features.json",
+    "yelp_dataset/yelp_academic_dataset_business.json",
+    "yelp_dataset/yelp_academic_dataset_checkin.json",
+    "yelp_dataset/yelp_academic_dataset_tip.json",
+    "yelp_dataset/yelp_academic_dataset_user.json"
+]
