5c608545c8b8b6ead36039e0403a607d970c1b89
PietroGuardati <pietroguardati@gmail.com>
2021-03-19 00:25:36 +0100

    proto: complete etl from ods to dwh

--- proto_postgres/etl_ods2dwh.sql
+++ proto_postgres/etl_ods2dwh.sql
@@ -1,4 +1,4 @@
---merge weather data
+-- fill dimension tables
 insert into star_dim_weather
 select
     p.weather_date,
@@ -10,8 +10,45 @@ select
     normal_max
 from ods_precipitations as p
 join ods_temperatures as t
-on p.weather_date = t.weather_date
+on p.weather_date = t.weather_date;
 
---delete review_date seconds in reviews
+insert into star_dim_business
+select
+	 business_id,
+	 business_name,
+	 city,
+	 stars,
+	 review_count
+from ods_business_features;
 
---
+insert into star_dim_users
+select
+	 user_id,
+	 yelping_since,
+	 user_name,
+	 average_stars,
+	 review_count
+from ods_users;
+
+-- fill fact table (use join to enforce data integrity)
+insert into star_fact_reviews
+with r as (
+    select
+        review_id,
+        date_trunc('day',review_date) as review_date,
+        business_id,
+        user_id,
+        stars,
+        useful,
+        funny,
+        cool,
+        review_text
+    from ods_reviews
+)
+select r.* from r
+join star_dim_weather as w
+on r.review_date = w.weather_date
+join star_dim_business as b
+on r.business_id = b.business_id
+join star_dim_users as u
+on r.user_id = u.user_id
