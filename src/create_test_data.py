efce4c8fadb147a44b2c306e0bcd67d838044cde
PietroGuardati <pietroguardati@gmail.com>
2021-03-18 01:44:27 +0100

    proto: intermediate step - sample business data, not fully working for one dataset

--- src/create_test_data.py
+++ src/create_test_data.py
@@ -58,14 +58,14 @@ def sample_reviews(dataset_path):
     return reviews_id, business_id, user_id, date
 
 
-def sample_weather_data(dataset_path, number_of_elements=100):
+def sample_weather_data(dataset_path, dates, number_of_elements=100):
     input_file = os.path.join(DIR_DATA, dataset_path)
     output_file = os.path.join(DIR_DATA_TEST, dataset_path)
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
     df = pd.read_csv(input_file)
     # get dates that matches reviews dates
-    df_date = pd.DatetimeIndex(date).strftime('%Y%m%d')
+    df_date = pd.DatetimeIndex(dates).strftime('%Y%m%d')
     target_dates = list(df_date.astype(int).values)
     df = df[df.date.isin(target_dates)]
     number_of_elements = len(df) if (len(df) < number_of_elements) \
@@ -73,6 +73,22 @@ def sample_weather_data(dataset_path, number_of_elements=100):
     # get maximum elements
     df = df.sample(number_of_elements)
     df.to_csv(output_file, index=False)
+    return df
+
+
+def sample_business_data(dataset_path, business_ids, number_of_elements=100):
+    input_file = os.path.join(DIR_DATA, dataset_path)
+    output_file = os.path.join(DIR_DATA_TEST, dataset_path)
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
+    df = pd.read_json(input_file, lines=True)
+    # get dates that matches reviews dates
+    df = df[df.business_id.isin(business_ids)]
+    # get maximum elements
+    number_of_elements = len(df) if (len(df) < number_of_elements) \
+        else number_of_elements
+    df = df.sample(number_of_elements)
+    df.to_json(output_file, orient="records")
     return df
 
 
@@ -95,7 +111,7 @@ def sample_random_json_data(dataset_path, number_of_elements=100):
 
 
 reviews_path = "yelp_dataset/yelp_academic_dataset_review.json"
-reviews_id, business_id, user_id, date = sample_reviews(reviews_path)
+reviews_ids, business_ids, user_ids, dates = sample_reviews(reviews_path)
 datasets = [
     "climate_explorer/USC00420849-BOULDER-precipitation-inch.csv",
     "climate_explorer/USC00420849-temperature-degreeF.csv",
@@ -106,9 +122,15 @@ datasets = [
     "yelp_dataset/yelp_academic_dataset_user.json"
 ]
 
-_ = sample_weather_data(datasets[0])
-_ = sample_weather_data(datasets[1])
+_ = sample_weather_data(datasets[0], dates)
+_ = sample_weather_data(datasets[1], dates)
+df = sample_business_data(datasets[2], business_ids) # TODO -> this is empty, no common ids with reviews?
+df = sample_business_data(datasets[3], business_ids)
 _ = sample_random_json_data(datasets[4])
 _ = sample_random_json_data(datasets[6])
 
+# sample json by business_id
+dataset_path = datasets[4]
+number_of_elements = 100
 
+# TODO1: fix business id (2), fix user_id (7), encapsulate and use
