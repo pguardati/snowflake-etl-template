c7424f235f025882c5cd288da89d453085ce1868
PietroGuardati <pietroguardati@gmail.com>
2021-03-18 02:17:36 +0100

    proto: allow script to create test data with same foreign and primary keys

--- src/create_test_data.py
+++ src/create_test_data.py
@@ -4,39 +4,7 @@ import pandas as pd
 from src.constants import DIR_DATA, DIR_DATA_TEST
 
 
-# def sample_dataset(dataset_path, target_data, target_column):
-#     """Sample and store a dataset
-#
-#     Args:
-#         dataset_path(str): dataset path respect to DIR_DATA
-#
-#     """
-#     input_file = os.path.join(DIR_DATA, dataset_path)
-#     output_file = os.path.join(DIR_DATA_TEST, dataset_path)
-#     os.makedirs(os.path.dirname(output_file), exist_ok=True)
-#
-#     # sample and store file in test directory
-#     extension = dataset_path.split(".")[-1]
-#     if extension == "csv":
-#         df = pd.read_csv(
-#             input_file,
-#         )
-#         df.to_csv(output_file, index=False)
-#     elif extension == "json":
-#         df = pd.read_json(
-#             input_file,
-#             lines=True
-#         )
-#         df.to_json(
-#             output_file,
-#             orient="records"
-#         )
-#
-#     print(f"storing dataset in {output_file}")
-
-
-def sample_reviews(dataset_path):
-    number_of_elements = 100
+def sample_reviews(dataset_path, number_of_elements=100):
     input_file = os.path.join(DIR_DATA, dataset_path)
     output_file = os.path.join(DIR_DATA_TEST, dataset_path)
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
@@ -55,6 +23,8 @@ def sample_reviews(dataset_path):
     business_id = list(df["business_id"].values)
     user_id = list(df["user_id"].values)
     date = list(df["date"].values)
+
+    print(f"exported {len(df)} elements in {dataset_path}")
     return reviews_id, business_id, user_id, date
 
 
@@ -73,22 +43,28 @@ def sample_weather_data(dataset_path, dates, number_of_elements=100):
     # get maximum elements
     df = df.sample(number_of_elements)
     df.to_csv(output_file, index=False)
+    print(f"exported {len(df)} elements in {dataset_path}")
     return df
 
 
-def sample_business_data(dataset_path, business_ids, number_of_elements=100):
+def sample_data_by_target(
+        dataset_path,
+        target_ids,
+        target_column,
+        number_of_elements=100
+):
     input_file = os.path.join(DIR_DATA, dataset_path)
     output_file = os.path.join(DIR_DATA_TEST, dataset_path)
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
-    df = pd.read_json(input_file, lines=True)
+    df = pd.read_json(input_file, lines=True, nrows=number_of_elements)
     # get dates that matches reviews dates
-    df = df[df.business_id.isin(business_ids)]
+    df = df[df[target_column].isin(target_ids)]
     # get maximum elements
     number_of_elements = len(df) if (len(df) < number_of_elements) \
         else number_of_elements
-    df = df.sample(number_of_elements)
     df.to_json(output_file, orient="records")
+    print(f"exported {len(df)} elements in {dataset_path}")
     return df
 
 
@@ -98,39 +74,47 @@ def sample_random_json_data(dataset_path, number_of_elements=100):
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
     df = pd.read_json(input_file, lines=True, nrows=number_of_elements)
     df.to_json(output_file, orient="records")
+    print(f"exported {len(df)} elements in {dataset_path}")
     return df
 
 
-#
-# def main():
-#     """create test data"""
-#
-#
-# if __name__ == "__main__":
-#     main()
+def main():
+    """create test data"""
+    # Sample reviews - it contains all foreign keys of the future star schema
+    reviews_path = "yelp_dataset/yelp_academic_dataset_review.json"
+    reviews_ids, business_ids, user_ids, dates = sample_reviews(
+        reviews_path,
+        number_of_elements=3000
+    )
+
+    # Sample data that matches the extracted keys from reviews
+    datasets = [
+        "climate_explorer/USC00420849-BOULDER-precipitation-inch.csv",
+        "climate_explorer/USC00420849-temperature-degreeF.csv",
+
+        "covid_19_dataset_2020_06_10/yelp_academic_dataset_covid_features.json",
+
+        "yelp_dataset/yelp_academic_dataset_business.json",
+
+        "yelp_dataset/yelp_academic_dataset_checkin.json",
+        "yelp_dataset/yelp_academic_dataset_tip.json",
+
+        "yelp_dataset/yelp_academic_dataset_user.json"
+    ]
+
+    _ = sample_weather_data(datasets[0], dates)
+    _ = sample_weather_data(datasets[1], dates)
+
+    _ = sample_data_by_target(datasets[3], business_ids, "business_id")
+    _ = sample_random_json_data(datasets[2])
+    # fill at random and exclude from star
+
+    _ = sample_random_json_data(datasets[4])
+    _ = sample_random_json_data(datasets[5])
+
+    _ = sample_data_by_target(datasets[6], user_ids, "user_id",
+                              number_of_elements=1000)
 
 
-reviews_path = "yelp_dataset/yelp_academic_dataset_review.json"
-reviews_ids, business_ids, user_ids, dates = sample_reviews(reviews_path)
-datasets = [
-    "climate_explorer/USC00420849-BOULDER-precipitation-inch.csv",
-    "climate_explorer/USC00420849-temperature-degreeF.csv",
-    "covid_19_dataset_2020_06_10/yelp_academic_dataset_covid_features.json",
-    "yelp_dataset/yelp_academic_dataset_business.json",
-    "yelp_dataset/yelp_academic_dataset_checkin.json",
-    "yelp_dataset/yelp_academic_dataset_tip.json",
-    "yelp_dataset/yelp_academic_dataset_user.json"
-]
-
-_ = sample_weather_data(datasets[0], dates)
-_ = sample_weather_data(datasets[1], dates)
-df = sample_business_data(datasets[2], business_ids) # TODO -> this is empty, no common ids with reviews?
-df = sample_business_data(datasets[3], business_ids)
-_ = sample_random_json_data(datasets[4])
-_ = sample_random_json_data(datasets[6])
-
-# sample json by business_id
-dataset_path = datasets[4]
-number_of_elements = 100
-
-# TODO1: fix business id (2), fix user_id (7), encapsulate and use
+if __name__ == "__main__":
+    main()
