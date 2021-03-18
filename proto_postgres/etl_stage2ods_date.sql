74282a8820e0f47b5d0b3fd13c48b1cadd93556c
PietroGuardati <pietroguardati@gmail.com>
2021-03-18 22:23:09 +0100

    proto: refactor reviews

--- proto_postgres/etl_stage2ods_date.sql
+++ proto_postgres/etl_stage2ods_date.sql
@@ -4,28 +4,6 @@ insert into ods_checkins (
 select business_id
 from checkins;
 
-insert into ods_reviews (
-	 review_id,
-	 date,
-	 business_id,
-	 user_id,
-	 stars,
-	 useful,
-	 funny,
-	 cool,
-	 text
-)
-select
-	 review_id,
-	 to_timestamp(date,'YYYY-MM-DD HH24:mi:ss') as date,
-	 business_id,
-	 user_id,
-	 cast(stars as int),
-	 cast(useful as int),
-	 cast(funny as int),
-	 cast(cool as int),
-	 text
-from reviews;
 
 insert into ods_tips (
 	 date,
@@ -44,4 +22,3 @@ from tips;
 
 select * from ods_checkins;
 select * from ods_tips;
-select * from ods_reviews;
---
+++ proto_postgres/etl_stage2ods_users.sql
@@ -0,0 +1,24 @@
+insert into ods_reviews (
+	 review_id,
+	 date,
+	 business_id,
+	 user_id,
+	 stars,
+	 useful,
+	 funny,
+	 cool,
+	 text
+)
+select
+	 review_id,
+	 to_timestamp(date,'YYYY-MM-DD HH24:mi:ss') as date,
+	 business_id,
+	 user_id,
+	 cast(stars as int),
+	 cast(useful as int),
+	 cast(funny as int),
+	 cast(cool as int),
+	 text
+from reviews;
+
+select * from ods_reviews;
