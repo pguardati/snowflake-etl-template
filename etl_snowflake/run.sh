2ebd7dfb149ec0e00fc2ba1b163c91503d553894
PietroGuardati <pietroguardati@gmail.com>
2021-03-22 10:13:57 +0100

    proto: create script to run database creation and etl

--- etl_snowflake/run.sh
+++ etl_snowflake/run.sh
@@ -1,38 +1,24 @@
-# Setup snowflake 
-Insert credentials `~/.snowsql/config`
-Copy config in `config/snowflake.config`
-
 # Creation
-```
 snowsql \
 -f reset_db.sql \
 -f ods_ddl.sql \
 -f dwh_ddl.sql
 python staging_create_staging_area.py
-```
 
 # Staging
-```
 python staging_upload_json.py
 python staging_upload_csv.py
-```
 
 # ODS
-```
 snowsql \
 -f ods_etl_weather.sql \
 -f ods_etl_business.sql \
 -f ods_etl_date.sql \
 -f ods_etl_users.sql
-```
 
 # DWH
-```
 snowsql -f dwh_etl.sql \
-```
 
 # Business Query
-```
 snowsql -f dwh_query.sql
-```
 
